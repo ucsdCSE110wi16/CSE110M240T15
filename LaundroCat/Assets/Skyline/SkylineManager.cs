084cede243cc9bc7a43c8b44c135f84d1b7fb364
andynaguyen <andylikestonguyen@gmail.com>
2016-02-07 13:59:53 -0800

    Changed skyline to 2D
    yay

--- LaundroCat/Assets/Skyline/SkylineManager.cs
+++ LaundroCat/Assets/Skyline/SkylineManager.cs
@@ -8,18 +8,22 @@ public class SkylineManager : MonoBehaviour {
     public Vector3 startPos;
     private Vector3 nextPos;
 
-    void Start() { 
+    void Start()
+    {
         nextPos = startPos;
 
-        for (int i = 0; i < numberOfObjects; i++) {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
             Transform o = (Transform)Instantiate(prefab);
-          //  GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-          //  cube.AddComponent<Rigidbody>();
-          //  cube.transform.position = nextPos;
 
             o.localPosition = nextPos;
-            nextPos.x ++;
+            nextPos.x++;
             Debug.Log("nextPos = " + nextPos.x + ", localPos = " + o.localPosition.x);
         }
+    }
+
+    // build chunk 1
+    void buildTerrain1(Vector3 v) {
+
     }
 }
