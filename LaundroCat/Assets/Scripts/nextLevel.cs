6f55e47333356e823e3d6ac18434cdd03e5189f5
Nathan Ringelberg <nsringelberg@gmail.com>
2016-02-06 17:21:24 -0800

    Testing out next level triggers in test - copy

---
+++ LaundroCat/Assets/Scripts/nextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class nextLevel : MonoBehaviour {
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log ("FUCK");
+       /* if( col.name == "player_turtle")
+        {
+            SceneManager.LoadScene(0);
+        } */
+    }
+}
