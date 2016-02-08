0454a487a3a15ee7851e0acde09457b79f62dd80
Albert-Tang <altang@ucsd.edu>
2016-02-08 00:02:52 -0800

    Consolidated changes.

---
+++ LaundroCat/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    public GameObject loadingImage;
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
