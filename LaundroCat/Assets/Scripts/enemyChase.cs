667bc5aa6347d3818d47b022a280ec8370e3a434
Amitoj Setia <amitoj_setia@yahoo.com>
2016-02-10 15:59:51 -0800

    added enemy chase ai
    new script "enemyChase"- makes ground enemy follow player's x-axis
    movement

---
+++ LaundroCat/Assets/Scripts/enemyChase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyChase : MonoBehaviour {
+
+    Transform target;
+    public float speed = 1f;
+    private Rigidbody2D r;
+
+    // Use this for initialization
+    void Start () {
+        r = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        target = GameObject.FindWithTag("Player").transform;
+
+        Vector3 forwardAxis = new Vector3(0, 0, -1);
+
+        transform.LookAt(target.position, forwardAxis);
+        Debug.DrawLine(transform.position, target.position);
+        transform.eulerAngles = new Vector3(1, 0, 0);
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * 2 * Time.deltaTime);
+
+    }
+
+}
