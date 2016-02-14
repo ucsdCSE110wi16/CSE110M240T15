6a87fe1abd5bdeba2c9c1a39a68d48c2b00d19b4
rabelson <rabelson@ucsd.edu>
2016-02-13 19:00:18 -0800

    In the process of making weapons

---
+++ LaundroCat/Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    public bool weapon_beam = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Which powerup is being used;
+	    if (weapon_beam)
+        {
+            GetComponent<Animation>().Play();
+        }
+	}   
+}
