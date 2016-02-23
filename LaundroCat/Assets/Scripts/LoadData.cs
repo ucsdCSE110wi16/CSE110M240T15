0285607e2711e4f418b2ee95eeb4622db95ceef4
Albert-Tang <altang@ucsd.edu>
2016-02-22 23:12:09 -0800

    Modified settings menu and added sprites

---
+++ LaundroCat/Assets/Scripts/LoadData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LoadData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    if (!PlayerPrefs.HasKey("Socks"))
+        {
+            PlayerPrefs.SetFloat("Music", (float)1.0);
+            PlayerPrefs.SetFloat("Sound", (float)1.0);
+
+            PlayerPrefs.SetInt("Socks", 0);
+
+            PlayerPrefs.SetString("Cat", "true");
+            PlayerPrefs.SetString("Dino", "false");
+            PlayerPrefs.SetString("Duck", "false");
+            PlayerPrefs.SetString("Elephant", "false");
+            PlayerPrefs.SetString("Sheep", "false");
+            PlayerPrefs.SetString("Turtle", "false");
+            PlayerPrefs.Save();
+        }
+	}
+}
---
+++ LaundroCat/Assets/Scripts/SettingsMenuMethods.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SettingsMenuMethods : MonoBehaviour {
+
+    public GameObject backButton;
+	public GameObject musicButton;
+	public GameObject musicSlider;
+	public GameObject soundsButton;
+	public GameObject soundsSlider;
+	public GameObject creditsButton;
+	public GameObject creditsPanel;
+
+	public void goBack() {
+		Debug.Log(creditsPanel.activeSelf);
+		if (creditsPanel.activeSelf == true) {
+			creditsPanel.SetActive(false);
+		}
+		else {
+			SceneManager.LoadScene(0);
+		}
+	}
+	
+    public void returnToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void setMusicVolume(float value)
+    {
+        PlayerPrefs.SetFloat("Music", value);
+        PlayerPrefs.Save();
+    }
+
+    public void setSoundsVolume(float value)
+    {
+        PlayerPrefs.SetFloat("Sounds", value);
+        PlayerPrefs.Save();
+    }
+	
+	public void openCredits() {
+		creditsPanel.SetActive(true);
+	}
+}
