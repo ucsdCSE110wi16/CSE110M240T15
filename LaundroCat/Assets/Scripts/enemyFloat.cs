32e0870990af98786227c5a2f75aafc46a4fab9a
Amitoj Setia <amitoj_setia@yahoo.com>
2016-03-04 23:03:10 -0800

    haunted laundry sorta works
    it floats back and forth for 3 seconds at initial spawn height

--- LaundroCat/Assets/Scripts/enemyFloat.cs
+++ LaundroCat/Assets/Scripts/enemyFloat.cs
@@ -4,32 +4,43 @@ using System.Collections;
 public class enemyFloat : MonoBehaviour {
 
 	public float speed = 1f;
+    public float moveSpeed = 0.01f;
 
-	private Rigidbody2D r;
+    private float time = 3f;
+    private float toAdd = 3f;
+
+    private int toGo = -1;
+
+	//private Rigidbody2D r;
 	private gameMaster gm;
 	public LayerMask mask = -1;
 
 	public bool spawnStuff = true;
 
-	// Use this for initialization
-	void Start () {
-		r = gameObject.GetComponent<Rigidbody2D> ();
-		r.gravityScale = 0;
-		if (GameObject.FindGameObjectWithTag("GameMaster") != null)
-			gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
+    // Use this for initialization
+    void Start()
+    {
+        //r = gameObject.GetComponent<Rigidbody2D>();
+        //r.gravityScale = 0;
+        if (GameObject.FindGameObjectWithTag("GameMaster") != null)
+            gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
 
-		RaycastHit hit;
-		Ray ray = new Ray (transform.position + Vector3.up * 100, Vector3.down);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up * 100, Vector3.down);
 
-		if (Physics.Raycast (ray, out hit, Mathf.Infinity, mask)) {
-			if (hit.collider != null) {
-				transform.position = new Vector3 (transform.position.x, hit.point.y + 20f, transform.position.z);
-			}
-		}
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+        {
+            if (hit.collider != null)
+            {
+                transform.position = new Vector3(transform.position.x, hit.point.y + 20f, transform.position.z);
+            }
+        }
 
-		/* float newYPos = Terrain.activeTerrain.SampleHeight (transform.position) + 20f;
+        
+
+        /* float newYPos = Terrain.activeTerrain.SampleHeight (transform.position) + 20f;
 		transform.position = new Vector3 (transform.position.x, newYPos, transform.position.z);*/
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -41,8 +52,22 @@ public class enemyFloat : MonoBehaviour {
 				transform.position = new Vector3 (transform.position.x, hit.point.y + 20f, transform.position.z);
 			}
 		}
-			
-		if (gameObject == null) {
+
+        time -= Time.deltaTime;
+        //print(time);
+
+        transform.position = new Vector3 (transform.position.x + moveSpeed, transform.position.y, transform.position.z);
+
+        if (time <= 0)
+        {
+            time += toAdd;
+            moveSpeed = -moveSpeed;
+            Vector3 flipped = transform.localScale;
+            flipped.x *= -1;
+            transform.localScale = flipped;
+        }
+
+        if (gameObject == null) {
 			return;
 		}
 
