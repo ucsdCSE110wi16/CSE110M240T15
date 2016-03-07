897ddfc9b64bb66d99c501a15722f1c6f9190efc
andynaguyen <andylikestonguyen@gmail.com>
2016-03-07 01:57:17 -0800

    Fixed camera for reals
    freals

--- LaundroCat/Assets/Scripts/CameraFollow.cs
+++ LaundroCat/Assets/Scripts/CameraFollow.cs
@@ -13,12 +13,45 @@ public class CameraFollow : MonoBehaviour {
 
     public Vector3 minCameraPos;
     public Vector3 maxCameraPos;
+    public GameMenuScript gms;
+
 
 	// Use this for initialization
 	void Start () {
+        gms = GameObject.Find("EventSystem").GetComponent<GameMenuScript>();
+
+        gms.cat.active = false;
+        gms.dino.active = false;
+        gms.duck.active = false;
+        gms.elephant.active = false;
+        gms.sheep.active = false;
+        gms.turtle.active = false;
+
+        switch (PlayerPrefs.GetString("current")) {
+            case "cat":
+                gms.cat.SetActive(true);
+                break;
+            case "dino":
+                gms.dino.SetActive(true);
+                break;
+            case "duck":
+                gms.duck.SetActive(true);
+                break;
+            case "elephant":
+                gms.elephant.SetActive(true);
+                break;
+            case "sheep":
+                gms.sheep.SetActive(true);
+                break;
+            case "turtle":
+                gms.turtle.SetActive(true);
+                break;
+        }
         player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
+
+
 	// Update is called once per frame
 	void FixedUpdate()
     {
